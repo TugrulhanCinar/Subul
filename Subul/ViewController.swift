6bb0d4e70bf7ee0e92373c58cd0ec6b17deb5b5a
TugrulhanCinar <61706877+TugrulhanCinar@users.noreply.github.com>
2024-09-25 18:24:16 +0300

    Map added

--- Subul/ViewController.swift
+++ Subul/ViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class SubulViewController: UIViewController {
 
+    var mapView: MKMapView!
+    var locationManager: CLLocationManager!
+    
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView = MKMapView(frame: view.bounds)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(mapView)
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
 
 }
 
+// MARK: - CLLocationManagerDelegate
+
+extension SubulViewController: CLLocationManagerDelegate {
+    // Haritayı belirli bir lokasyona odakla
+    func centerMapOnLocation(location: CLLocation, regionRadius: CLLocationDistance = 1000) {
+        let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+
+    // Konum güncellemesi aldığında haritayı güncelle
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            centerMapOnLocation(location: location)
+        }
+    }
+}

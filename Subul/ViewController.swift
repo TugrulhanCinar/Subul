b0740069a6b31a85bcac546f2aac9b3ac2e50db8
TugrulhanCinar <61706877+TugrulhanCinar@users.noreply.github.com>
2024-09-25 20:11:53 +0300

    refactored route story

--- Subul/ViewController.swift
+++ Subul/ViewController.swift
@@ -10,80 +10,90 @@ import MapKit
 import CoreLocation
 
 class SubulViewController: UIViewController {
-
+    
     var mapView: MKMapView!
-      var locationManager: CLLocationManager!
-      var startAddressField: UITextField!
-      var destinationAddressField: UITextField!
-      var calculateRouteButton: UIButton!
-      
-      override func viewDidLoad() {
-          super.viewDidLoad()
-          
-          // MapView oluştur ve ekrana yerleştir
-          mapView = MKMapView(frame: CGRect(x: 0, y: 200, width: view.frame.size.width, height: view.frame.size.height - 200))
-          view.addSubview(mapView)
-          
-          // Kullanıcının konumunu göster
-          mapView.showsUserLocation = true
-          mapView.delegate = self
-          
-          // Konum yöneticisini başlat
-          locationManager = CLLocationManager()
-          locationManager.requestWhenInUseAuthorization()
+    var locationManager: CLLocationManager!
+    var selectedTransportType: MKDirectionsTransportType = .automobile
+    var currentRoute: MKPolyline?
 
-          // Başlangıç adresi için TextField
-          startAddressField = UITextField(frame: CGRect(x: 20, y: 50, width: view.frame.size.width - 40, height: 40))
-          startAddressField.placeholder = "Başlangıç adresi"
-          startAddressField.borderStyle = .roundedRect
-          view.addSubview(startAddressField)
-          
-          // Varış adresi için TextField
-          destinationAddressField = UITextField(frame: CGRect(x: 20, y: 100, width: view.frame.size.width - 40, height: 40))
-          destinationAddressField.placeholder = "Varış adresi"
-          destinationAddressField.borderStyle = .roundedRect
-          view.addSubview(destinationAddressField)
-          
-          // Rota hesapla butonu
-          calculateRouteButton = UIButton(frame: CGRect(x: 20, y: 150, width: view.frame.size.width - 40, height: 40))
-          calculateRouteButton.setTitle("Rota Hesapla", for: .normal)
-          calculateRouteButton.backgroundColor = .blue
-          calculateRouteButton.addTarget(self, action: #selector(calculateRoute), for: .touchUpInside)
-          view.addSubview(calculateRouteButton)
-      }
-      
-      @objc func calculateRoute() {
-          // Girilen adreslerin koordinatlarını al
-          guard let startAddress = startAddressField.text, let destinationAddress = destinationAddressField.text else { return }
-          // Mevcut işaretçileri kaldır
-          mapView.removeAnnotations(mapView.annotations)
-          getCoordinateFrom(address: startAddress) { (startCoordinate) in
-              self.getCoordinateFrom(address: destinationAddress) { (destinationCoordinate) in
-                  // Başlangıç ve bitiş noktalarına işaretçi ekle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "+", style: .plain, target: self, action: #selector(addressButtonTapped))
+        
+        mapView = MKMapView(frame: view.bounds)
+        mapView.delegate = self
+        view.addSubview(mapView)
+        
+        mapView.showsUserLocation = true
 
-                  self.addAnnotationToMap(coordinate: startCoordinate, title: "Başlangıç")
-                  self.addAnnotationToMap(coordinate: destinationCoordinate, title: "Bitiş")
-                  
-                  // Yaya rotasını göster
-                  self.showRouteOnMap(pickupCoordinate: startCoordinate, destinationCoordinate: destinationCoordinate, transportType: .walking)
-                  
-                  // Araç rotasını göster
-                  self.showRouteOnMap(pickupCoordinate: startCoordinate, destinationCoordinate: destinationCoordinate, transportType: .automobile)
-              }
-          }
-      }
-      
-      // Adresten koordinat al
-      func getCoordinateFrom(address: String, completion: @escaping(CLLocationCoordinate2D) -> Void) {
-          let geocoder = CLGeocoder()
-          geocoder.geocodeAddressString(address) { (placemarks, error) in
-              if let placemark = placemarks?.first, let location = placemark.location {
-                  completion(location.coordinate)
-              }
-          }
-      }
-      
-    // Koordinata işaretçi ekle
+        locationManager = CLLocationManager()
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    @objc func addressButtonTapped() {
+           let alertController = UIAlertController(title: "address", message: "\n\n\n\n\n", preferredStyle: .alert)
+           
+           alertController.addTextField { (textField) in
+               textField.placeholder = "Start address"
+           }
+           
+           alertController.addTextField { (textField) in
+               textField.placeholder = "destination address"
+           }
+           
+           let segmentedControl = UISegmentedControl(items: ["Walking", "automobile"])
+           segmentedControl.frame = CGRect(x: 10, y: 80, width: 250, height: 30)
+           segmentedControl.selectedSegmentIndex = 1 // Varsayılan olarak "Araba" seçili
+           
+           alertController.view.addSubview(segmentedControl)
+           
+           let submitAction = UIAlertAction(title: "Ok", style: .default) { [unowned self] _ in
+               let startAddress = alertController.textFields?[0].text
+               let endAddress = alertController.textFields?[1].text
+               
+               if segmentedControl.selectedSegmentIndex == 0 {
+                   self.selectedTransportType = .walking // Yaya
+               } else {
+                   self.selectedTransportType = .automobile // Araba
+               }
+               
+               if let start = startAddress, let end = endAddress {
+                   self.calculateRoute(startAddress: start, endAddress: end)
+               }
+           }
+           
+           let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+           
+           alertController.addAction(submitAction)
+           alertController.addAction(cancelAction)
+           
+           present(alertController, animated: true, completion: nil)
+       }
+    
+    func calculateRoute(startAddress: String, endAddress: String) {
+
+        mapView.removeAnnotations(mapView.annotations)
+        
+        getCoordinateFrom(address: startAddress) { (startCoordinate) in
+            self.getCoordinateFrom(address: endAddress) { (endCoordinate) in
+                self.addAnnotationToMap(coordinate: startCoordinate, title: "start")
+                self.addAnnotationToMap(coordinate: endCoordinate, title: "destination")
+                
+                self.showRouteOnMap(pickupCoordinate: startCoordinate, destinationCoordinate: endCoordinate, transportType: self.selectedTransportType)
+            }
+        }
+    }
+    
+    func getCoordinateFrom(address: String, completion: @escaping(CLLocationCoordinate2D) -> Void) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            if let placemark = placemarks?.first, let location = placemark.location {
+                completion(location.coordinate)
+            }
+        }
+    }
+    
     func addAnnotationToMap(coordinate: CLLocationCoordinate2D, title: String) {
         let annotation = MKPointAnnotation()
         annotation.coordinate = coordinate
@@ -91,39 +101,42 @@ class SubulViewController: UIViewController {
         mapView.addAnnotation(annotation)
     }
     
-      // Rota oluştur ve haritada göster
-      func showRouteOnMap(pickupCoordinate: CLLocationCoordinate2D, destinationCoordinate: CLLocationCoordinate2D, transportType: MKDirectionsTransportType) {
-          let pickupPlacemark = MKPlacemark(coordinate: pickupCoordinate)
-          let destinationPlacemark = MKPlacemark(coordinate: destinationCoordinate)
-          
-          let directionRequest = MKDirections.Request()
-          directionRequest.source = MKMapItem(placemark: pickupPlacemark)
-          directionRequest.destination = MKMapItem(placemark: destinationPlacemark)
-          directionRequest.transportType = transportType
-          
-          let directions = MKDirections(request: directionRequest)
-          directions.calculate { (response, error) in
-              guard let response = response else {
-                  print("Rota hesaplanamadı: \(String(describing: error))")
-                  return
-              }
-              
-              let route = response.routes[0]
-                            
-              self.mapView.addOverlay(route.polyline, level: MKOverlayLevel.aboveRoads)
-              self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
-          }
+    func showRouteOnMap(pickupCoordinate: CLLocationCoordinate2D, destinationCoordinate: CLLocationCoordinate2D, transportType: MKDirectionsTransportType) {
+        let pickupPlacemark = MKPlacemark(coordinate: pickupCoordinate)
+        let destinationPlacemark = MKPlacemark(coordinate: destinationCoordinate)
+        
+        let directionRequest = MKDirections.Request()
+        directionRequest.source = MKMapItem(placemark: pickupPlacemark)
+        directionRequest.destination = MKMapItem(placemark: destinationPlacemark)
+        directionRequest.transportType = transportType
+        
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate { (response, error) in
+            guard let response = response else {
+                print("Rota hesaplanamadı: \(String(describing: error))")
+                return
+            }
+            
+            // Önceki rotayı kaldır
+                    if let existingRoute = self.currentRoute {
+                        self.mapView.removeOverlay(existingRoute)
+                    }
+            
+            let route = response.routes[0]
+            self.currentRoute = route.polyline 
+            self.mapView.addOverlay(route.polyline, level: MKOverlayLevel.aboveRoads)
+            self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
         }
+    }
     
     }
 
 extension SubulViewController: MKMapViewDelegate {
-    // Rota çizgilerini haritada göster
+
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         if overlay is MKPolyline {
             let polylineRenderer = MKPolylineRenderer(overlay: overlay)
             
-            // Yaya rotası kırmızı, araç rotası mavi olacak
             polylineRenderer.strokeColor = (overlay as! MKPolyline).title == "Walking" ? UIColor.red : UIColor.blue
             polylineRenderer.lineWidth = 5
             return polylineRenderer
@@ -131,7 +144,6 @@ extension SubulViewController: MKMapViewDelegate {
         return MKOverlayRenderer()
     }
     
-    // İşaretçileri özelleştir ve özel ikon ekle
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         if annotation is MKUserLocation {
             return nil
@@ -144,11 +156,10 @@ extension SubulViewController: MKMapViewDelegate {
             annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
             annotationView?.canShowCallout = true
             
-            let image = annotation.title == "Başlangıç" ? CustomIcon.startIcon.getImage() : CustomIcon.finishICon.getImage()
+            let image = annotation.title == "start" ? CustomIcon.startIcon.getImage() : CustomIcon.finishICon.getImage()
             
             annotationView?.image = image.resizeImage(to: CGSize(width: 15, height: 15))
             
-            // İsteğe bağlı olarak çağrı kartına bir buton ekle
             let calloutButton = UIButton(type: .detailDisclosure)
             annotationView?.rightCalloutAccessoryView = calloutButton
         } else {
@@ -160,7 +171,7 @@ extension SubulViewController: MKMapViewDelegate {
 }
 
 extension UIImage {
-    // Görseli yeniden boyutlandırma fonksiyonu
+
     func resizeImage(to size: CGSize) -> UIImage? {
         let renderer = UIGraphicsImageRenderer(size: size)
         let resizedImage = renderer.image { _ in
